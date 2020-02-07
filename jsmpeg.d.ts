586991a9ec7712103cada18bdcbf459d00a2a7e7
Hotze Visser <hvisser@senk.local>
2020-02-07 15:59:50 +0100

    correct typings file

--- jsmpeg.d.ts
+++ jsmpeg.d.ts
@@ -58,7 +58,7 @@ declare module "jsmpeg" {
     onEnded?: (player: Player) => void;
     onStalled?: (player: Player) => void;
     onSourceEstablished?: (source: any) => void;
-    onSourceEstablished?: (source: any) => void;
+    onSourceCompleted?: (source: any) => void;
   }
 
   export interface VideoSource {
